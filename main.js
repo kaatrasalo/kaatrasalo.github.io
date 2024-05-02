a5da53a4da304549335894f79803bfce973e8a03
NuclideK <88620225+NuclideK@users.noreply.github.com>
2024-05-02 09:58:51 +0300

    nah i'd win

--- main.js
+++ main.js
@@ -7,18 +7,10 @@ const postclickDom = document.getElementById("post-click");
 const description = document.getElementById("description");
 const credits = document.getElementById("credits");
 
-function calculateAge() {
-  var ageDifMs = Date.now() - new Date("Mon, 06 Mar 2006 00:00:00 +0200");
-  var ageDate = new Date(ageDifMs);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
 let waiter = 0;
 function init() {
   preclickDom.classList.add("hidden");
   postclickDom.classList.add("hidden");
-  credits.classList.add("hidden");
-  credits.classList.add("gone");
 
   waiter = setInterval(() => {
     if (song.readyState == 4) {
@@ -40,10 +32,18 @@ async function main() {
   playing = true;
   preclickDom.classList.add("hidden");
   song.currentTime = 0;
-  song.volume = 0.15;
+  song.volume = 0.2;
   song.loop = true;
   song.play();
   postclickDom.classList.remove("hidden");
+  // Set up FlipDown
+  var flipdown = new FlipDown(1714654800)
+    // Start the countdown
+    .start()
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      window.location.href = "https://youtu.be/5JqY-6q-RNA?si=H9p6Ie2xZCzVKJbU";
+    });
 }
 async function togglecredits() {
   description.classList.toggle("hidden");
