5e7e65c9fb91f36d8ad815125c9c3b09510ec7a8
NuclideK <88620225+NuclideK@users.noreply.github.com>
2024-05-02 19:28:09 +0300

    Revert to original

---
+++ audiovisualizer.js
@@ -0,0 +1,25 @@
+import AudioMotionAnalyzer from "https://cdn.skypack.dev/audiomotion-analyzer?min";
+let height = window.screen.height;
+let width = window.screen.width;
+const audioMotion = new AudioMotionAnalyzer(document.getElementById("bg"), {
+  source: document.getElementById("song"),
+  height: height,
+  colorMode: "gradient",
+  showScaleX: false,
+  lumiBars: true,
+  mode: 1,
+  frequencyScale: "log",
+  channelLayout: "single",
+  smoothing: 0,
+  bgAlpha: 1,
+  overlay: true,
+});
+
+audioMotion.registerGradient("phoenix", {
+  bgColor: "#090909",
+  colorStops: ["#c77335"],
+});
+
+audioMotion.setOptions({
+  gradient: "phoenix",
+});
