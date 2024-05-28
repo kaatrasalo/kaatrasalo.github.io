69e2bb3936c049c13007db102b935916835f1128
NuclideK <88620225+NuclideK@users.noreply.github.com>
2024-05-28 21:53:32 +0300

    forget

--- audiovisualizer.js
+++ audiovisualizer.js
@@ -4,20 +4,43 @@ let width = window.screen.width;
 const audioMotion = new AudioMotionAnalyzer(document.getElementById("bg"), {
   source: document.getElementById("song"),
   height: height,
-  colorMode: "gradient",
+  colorMode: "bar-level",
+  roundBars: false,
   showScaleX: false,
-  lumiBars: true,
+  lumiBars: false,
   mode: 1,
   frequencyScale: "log",
   channelLayout: "single",
   smoothing: 0,
+  showPeaks: false,
   bgAlpha: 1,
   overlay: true,
 });
+window.audioMotion = audioMotion;
 
 audioMotion.registerGradient("phoenix", {
   bgColor: "#090909",
   colorStops: ["#c77335"],
+});
+audioMotion.registerGradient("emo", {
+  bgColor: "#090909",
+  colorStops: ["#6900FF", "#FFFFFF", "#FFFFFF"],
+});
+audioMotion.registerGradient("blood", {
+  bgColor: "#090909",
+  colorStops: [
+    "#8A0303", // Dark Blood Red
+    "#7E0000", // Deep Blood Red
+    "#9A0505", // Rich Blood Red
+    "#B22222", // Firebrick
+    "#8B0000", // Dark Red
+    "#A52A2A", // Brownish Red
+    "#DC143C", // Crimson
+    "#770000", // Deep Dark Red
+    "#990000", // Strong Red
+    "#660000", // Very Dark Red
+    "#5C0000", // Almost Black Red
+  ],
 });
 
 audioMotion.setOptions({
---
+++ v1/main.js
@@ -0,0 +1,54 @@
+let playing = false;
+const song = document.getElementById("song");
+
+const preclickDom = document.getElementById("pre-click");
+const postclickDom = document.getElementById("post-click");
+
+const description = document.getElementById("description");
+const credits = document.getElementById("credits");
+
+function calculateAge() {
+  var ageDifMs = Date.now() - new Date("Mon, 06 Mar 2006 00:00:00 +0200");
+  var ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+let waiter = 0;
+function init() {
+  preclickDom.classList.add("hidden");
+  postclickDom.classList.add("hidden");
+  credits.classList.add("hidden");
+  credits.classList.add("gone");
+
+  waiter = setInterval(() => {
+    if (song.readyState == 4) {
+      clearInterval(waiter);
+      waiter = null;
+      preclickDom.classList.remove("hidden");
+      console.log("Everything ready!");
+    }
+  }, 10);
+}
+async function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+async function main() {
+  if (playing) return;
+  if (waiter) return;
+  playing = true;
+  preclickDom.classList.add("hidden");
+  song.currentTime = 0;
+  song.volume = 0.15;
+  song.loop = true;
+  song.play();
+  postclickDom.classList.remove("hidden");
+}
+async function togglecredits() {
+  description.classList.toggle("hidden");
+  description.classList.toggle("gone");
+  credits.classList.toggle("gone");
+  credits.classList.toggle("hidden");
+}
+init();
